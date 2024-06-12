9f04cf5ec85ddec9fa1a9b9a551bc2b0087dd677
AgustinTroncoso <a.troncoso09@ufromail.cl>
2024-06-12 14:08:14 -0400

    Initial commit

---
+++ src/main/java/com/example/ufro/UfroApplication.java
@@ -0,0 +1,12 @@
+package com.example.ufro;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfroApplication.class, args);
+	}
+
+}
